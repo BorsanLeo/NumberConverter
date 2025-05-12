db0b70545f176044a8a67d09353ec14ada452e6f
BorsanLeo <leonardo.borsan@gmail.com>
2025-05-12 15:04:51 +0200

    implementata stampa fino al numero 100, e il relativo test, close #13

--- roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
+++ roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -16,6 +16,7 @@ public class RomanPrinter {
         // Definisci la rappresentazione ASCII art orizzontale per ogni lettera romana
         String[][] asciiLetters = {
             {//Riga1
+                "   _____ ",  // C
                 "  _      ",  // L
                 " __   __",  // X
                 " __      __",  // V
@@ -23,6 +24,7 @@ public class RomanPrinter {
             },
 
             {//Riga2
+                "  / ____|",  // C
                 " | |     ",  // L
                 " \\ \\ / /",  // X
                 " \\ \\    / /",  // V
@@ -30,6 +32,7 @@ public class RomanPrinter {
             },
 
             {//Riga3
+                " | |     ",  // C
                 " | |     ",  // L
                 "  \\ V / ",  // X
                 "  \\ \\  / / ",  // V
@@ -37,6 +40,7 @@ public class RomanPrinter {
             },
 
             {//Riga4
+                " | |     ", // C
                 " | |     ", // L
                 "   > <  ", // X
                 "   \\ \\/ /  ", // V
@@ -44,6 +48,7 @@ public class RomanPrinter {
             },
                 
             {//Riga5
+                " | |____ ", // C
                 " | |____ ", // L
                 "  / . \\ ", // X
                 "    \\  /   ", // V
@@ -51,6 +56,7 @@ public class RomanPrinter {
             },
                 
             {//Riga6
+                "  \\_____|", // C
                 " |______|", // L
                 " /_/ \\_\\", // X
                 "     \\/    ", // V
@@ -61,7 +67,7 @@ public class RomanPrinter {
         for( int z=0; z<6; z++) {
             for (char c : romanNumber.toCharArray()) {
                 // Aggiungi la rappresentazione ASCII art orizzontale della lettera corrente
-                asciiArt.append(asciiLetters[z]["LXVI".indexOf(c)]);
+                asciiArt.append(asciiLetters[z]["CLXVI".indexOf(c)]);
             }
             asciiArt.append("\n");
         }
